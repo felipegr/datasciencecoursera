ed4bd2ea096deee8819da916d1f8cb8da1aa45ee
felipegr <felipe.rosario@gmail.com>
2014-07-23 21:14:13 -0300

    Adding documentation

--- DevelopingDataProducts/ui.R
+++ DevelopingDataProducts/ui.R
@@ -8,6 +8,9 @@ shinyUI(pageWithSidebar(
     sliderInput('UIspeed', 'Set the speed', value = 15, min = 5, max = 25, step = 1,)
   ),
   mainPanel(
-    plotOutput('newPlot')
+    plotOutput('newPlot'),
+    helpText("The data give the speed of cars and the distances taken to stop and were recorded in the 1920s. It's a data frame with 50 observations on 2 variables."),
+    helpText("The line represents a linear model that's used to predict the braking distance of a car according to its speed."),
+    helpText("Use the slider to set the desired speed and the application returns the predicted braking distance as a black point in the graph and as text.")
   )
 ))
